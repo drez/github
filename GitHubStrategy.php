001330d279f4cbe2596c3faf9488f6273593d77f
Fred <fred@apigoat.com>
2020-10-22 15:18:42 +0800

    access_token in Authorization header

--- GitHubStrategy.php
+++ GitHubStrategy.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * GitHub strategy for Opauth
  * 
@@ -15,30 +16,32 @@
  * 
  * @package			Opauth.GitHub
  */
-class GitHubStrategy extends OpauthStrategy {
-	
+class GithubStrategy extends OpauthStrategy
+{
+
 	/**
 	 * Compulsory config keys, listed as unassociative arrays
 	 */
 	public $expects = array('client_id', 'client_secret');
-	
+
 	/**
 	 * Optional config keys, without predefining any default values.
 	 */
 	public $optionals = array('redirect_uri', 'scope', 'state');
-	
+
 	/**
 	 * Optional config keys with respective default values, listed as associative arrays
 	 * eg. array('scope' => 'email');
 	 */
 	public $defaults = array(
-		'redirect_uri' => '{complete_url_to_strategy}oauth2callback'
+		'redirect_uri' => _SITE_URL . "oauth/github/oauth2callback"
 	);
-	
+
 	/**
 	 * Auth request
 	 */
-	public function request() {
+	public function request()
+	{
 		$url = 'https://github.com/login/oauth/authorize';
 		$params = array(
 			'client_id' => $this->strategy['client_id'],
@@ -48,18 +51,19 @@ class GitHubStrategy extends OpauthStrategy {
 		foreach ($this->optionals as $key) {
 			if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
 		}
-		
+
 		$this->clientGet($url, $params);
 	}
-	
+
 	/**
 	 * Internal callback, after OAuth
 	 */
-	public function oauth2callback() {
+	public function oauth2callback()
+	{
 		if (array_key_exists('code', $_GET) && !empty($_GET['code'])) {
 			$code = $_GET['code'];
 			$url = 'https://github.com/login/oauth/access_token';
-			
+
 			$params = array(
 				'code' => $code,
 				'client_id' => $this->strategy['client_id'],
@@ -67,13 +71,13 @@ class GitHubStrategy extends OpauthStrategy {
 				'redirect_uri' => $this->strategy['redirect_uri'],
 			);
 			if (!empty($this->strategy['state'])) $params['state'] = $this->strategy['state'];
-			
+
 			$response = $this->serverPost($url, $params, null, $headers);
 			parse_str($response, $results);
-			
+
 			if (!empty($results) && !empty($results['access_token'])) {
 				$user = $this->user($results['access_token']);
-				
+
 				$this->auth = array(
 					'uid' => $user['id'],
 					'info' => array(),
@@ -82,7 +86,7 @@ class GitHubStrategy extends OpauthStrategy {
 					),
 					'raw' => $user
 				);
-				
+
 				$this->mapProfile($user, 'name', 'info.name');
 				$this->mapProfile($user, 'blog', 'info.urls.blog');
 				$this->mapProfile($user, 'avatar_url', 'info.image');
@@ -92,10 +96,9 @@ class GitHubStrategy extends OpauthStrategy {
 				$this->mapProfile($user, 'email', 'info.email');
 				$this->mapProfile($user, 'location', 'info.location');
 				$this->mapProfile($user, 'url', 'info.urls.github_api');
-				
+
 				$this->callback();
-			}
-			else {
+			} else {
 				$error = array(
 					'code' => 'access_token_error',
 					'message' => 'Failed when attempting to obtain access token',
@@ -107,30 +110,29 @@ class GitHubStrategy extends OpauthStrategy {
 
 				$this->errorCallback($error);
 			}
-		}
-		else {
+		} else {
 			$error = array(
 				'code' => 'oauth2callback_error',
 				'raw' => $_GET
 			);
-			
+
 			$this->errorCallback($error);
 		}
 	}
-	
+
 	/**
 	 * Queries GitHub v3 API for user info
 	 *
 	 * @param string $access_token 
 	 * @return array Parsed JSON results
 	 */
-	private function user($access_token) {
-		$user = $this->serverGet('https://api.github.com/user', array('access_token' => $access_token), null, $headers);
+	private function user($access_token)
+	{
+		$user = $this->serverGet('https://api.github.com/user', array('access_token' => $access_token), ['http' => ['header' => ["Authorization: token " . $access_token]]], $headers);
 
 		if (!empty($user)) {
 			return $this->recursiveGetObjectVars(json_decode($user));
-		}
-		else {
+		} else {
 			$error = array(
 				'code' => 'userinfo_error',
 				'message' => 'Failed when attempting to query GitHub v3 API for user information',
